c0572ad7d49afdd3e5637843e6219994bd9713bb
ees4dd <ees4dd@virginia.edu>
2015-04-27 14:26:36 -0400

    removed static_path

--- project_prototype/settings.py
+++ project_prototype/settings.py
@@ -108,13 +108,14 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
 
-STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
+#STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
 
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
+
     os.path.join(BASE_DIR, 'static'),
 )
 
